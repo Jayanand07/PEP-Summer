278f467f2a638e3ee84c296a271fabf6b7cf37cd
Jay Anand <anand.jay426344@gmail.com>
2026-06-24 13:00:03 +0530

    First half 24 June

---
+++ 24June/TypeScript/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let x = 10;
+let str = "Hello, TypeScript!";
+let isActive = true;
+//# sourceMappingURL=index.js.map
---
+++ 24June/TypeScript/index.ts
@@ -0,0 +1,3 @@
+let x: number = 10;
+let str : string = "Hello, TypeScript!";
+let isActive: boolean = true;
