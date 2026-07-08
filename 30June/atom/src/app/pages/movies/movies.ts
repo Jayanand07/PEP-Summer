b11aa72cfa682c5c5cb5e574a9d8fbf37a8533c0
Jay Anand <anand.jay426344@gmail.com>
2026-07-08 12:34:07 +0530

    Added Generic Global Http Methods with Service for scale

--- 30June/atom/src/app/pages/movies/movies.ts
+++ 30June/atom/src/app/pages/movies/movies.ts
@@ -1,6 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { Button } from '../../shared/button/button';
 import { buttonConfig, users } from '../../utils/utils';
+import { HttpClient } from '@angular/common/http';
+import { MovieService } from '../../services/movie.service';
 
 @Component({
   selector: 'app-movies',
@@ -9,23 +11,32 @@ import { buttonConfig, users } from '../../utils/utils';
   styleUrl: './movies.css',
 })
 export class Movies {
+  private http = inject(HttpClient);
+  private movieServices = inject(MovieService);
   currPage = 0;
   moviesArr = signal([]);
   buttonConfig = buttonConfig;
 
   fetchMovies(pageNo: number) {
-    this.currPage = pageNo;
-
-    const movieApiLink = `https://api.themoviedb.org/3/movie/now_playing?api_key=825866893c60ba5cbf0e6718bdd67310&language=en-US&page=${this.currPage}`;
-    fetch(movieApiLink)
-      .then((response) => response.json())
-      .then((data) => {
-        this.moviesArr.set(data.results);
-        console.log(this.moviesArr);
-      });
+    this.currPage = this.currPage + 1;
+    this.movieServices.getNowPlaying(this.currPage).subscribe((data : any) => {
+      this.moviesArr.set(data?.results);
+    })
   }
 
+  // fetchMovies(pageNo: number) {
+  //   this.currPage = pageNo;
+
+  //   const movieApiLink = `https://api.themoviedb.org/3/movie/now_playing?api_key=825866893c60ba5cbf0e6718bdd67310&language=en-US&page=${this.currPage}`;
+  //   fetch(movieApiLink)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       this.moviesArr.set(data.results);
+  //       console.log(this.moviesArr);
+  //     });
+  // }
+
   ngOnInit() {
-    this.fetchMovies(1);
+    this.fetchMovies(0);
   }
 }
